6c71cb2e17aebf1cce6197ae4ee8aaca8c98c5b7
aoliao12312 <l2536758201@outlook.com>
2024-09-23 08:24:04 +0800

    others add

--- C++/Hello_World.cpp
+++ C++/Hello_World.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
+
+template <class T>
+class Node {
+    public:
+    T data;
+    Node<T>* lchild, * rchild;
+    Node() {
+        lchild = nullptr;
+        rchild = nullptr;
+    }
+};
+template <class T>
+class Tree {
+    public:
+    Node<T>* root;
+    Tree() {
+        root = new Node<T>();
+    }
+
+};
+
 int main() {
-    cout << "Hello World!" << endl;
+
     return 0;
 }
